f57c7859a7618f6aa08f218d038ad88b8674e9bd
Sam Boero <sam.l.boero@gmail.com>
2018-11-04 19:09:44 +0100

    Deletion works . adding new ones not

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -39,16 +39,41 @@ router.get('/celebrities',  (req, res, next) => {
   });
 
   router.get('/new-celeb', (req, res, next) => {
-    // The simple render without the publishers
     res.render('new-celeb')
   
-    // Publisher.find({}, null, { sort: { name: 1 } })
-    //   .then(publishers => {
-    //     res.render('add-book', { publishers })
-    //   })
   })
   
+  //this is not working - form doesnt save to the database.....
+  router.post('/new-celeb', (req, res, next) => {
   
+   // Create a celebrity in the db with the information from the form
+    Celebrity.create({
+      name: req.body.name,
+      occupation: req.body.occupation,
+      catchPhrase: req.body.catchPhrase,
+    })
+    .then(celebrity => {
+      celebrity.save(function (err, celebrity) {
+        if (err) {Console.log("AHHHHHHH error")} })
+        res.redirect('/celebrities/' + celebrity._id)
+      })
+      .catch((err) => {
+       console.log("there was an error adding your celeb")
+       res.redirect("new-celeb")
+      })
+  })
+  
+
+router.post('/celebrities/:id/delete', (req, res, next) => {
+ // let id = req.params.id
+Celebrity.findByIdAndRemove(req.params.id)
+.then (celebsFromDb => {
+  res.redirect('/celebrities')
+})
+.catch((error) => {
+  next(error)
+})
+})
 
 
 
